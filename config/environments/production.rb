14ee94d12e3e231cf5c2f489d1a457b920f3dfd0
vamsidhar42 <vamsidharreddybada@gmail.com>
2016-02-02 18:18:11 +0530

    --

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_urls_options = { host: 'https://houseofcodes.herokuapp.com' }
+  config.action_mailer.default_urls_options = { host: 'https://houseofcodes.herokuapp.com' , port: 3000 }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
