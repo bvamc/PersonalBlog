4e4ee318c4285b8615cd4234c0ca75a44c6672e0
vamsidhar42 <vamsidharreddybada@gmail.com>
2016-02-02 08:16:29 +0530

    Add edit & destroy project abilities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,6 +22,23 @@ class ProjectsController < ApplicationController
 
 	end
 
+	def edit
+
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project , notice: "Nice Vamsi! That project was succesfully updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+
 	private
 
 	def project_params
