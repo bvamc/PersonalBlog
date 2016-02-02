db156b703e5cd57c054abb9753ce39fe8c86b30e
vamsidhar42 <vamsidharreddybada@gmail.com>
2016-02-02 06:38:01 +0530

    Added friendly_gem & setup friendly_id on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,11 +44,11 @@ def destroy
 	private
 
 	def post_params
-		params.require(:post).permit(:title, :content)
+		params.require(:post).permit(:title, :content, :slug)
 	end
 
 	def find_post
-		@post = Post.find(params[:id])
+		@post = Post.friendly.find(params[:id])
 	end
 	
 end
